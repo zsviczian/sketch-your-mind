f815a87598fca708f73fb5a260dd4bb3eb4a4b73
zsviczian <viczian.zsolt@gmail.com>
2025-09-12 09:20:35 +0000

    Nick Sousanis

--- 2025/config.js
+++ 2025/config.js
@@ -20,7 +20,7 @@
     3: {
        "1": { link: "sessions/jorge",     title: "How I Write My Books",     presenter: "Jorge Arango", zoomLink: "" },
        "2": { link: "sessions/jono",  title: "Sketchplanations",  presenter: "Jono Hey", zoomLink: "" },
-      // "3": { link: "sessions/session-twelve.html",  title: "Session Twelve",  presenter: "John Doe", zoomLink: "" }
+       "3": { link: "sessions/sousanis",  title: "Thinking in Comics",  presenter: "Nick Sousanis", zoomLink: "" }
     },
     4: {
       "1": { link: "sessions/ingrid", title: "Find Your Message", presenter: "Ingrid Lill", zoomLink: "" },
